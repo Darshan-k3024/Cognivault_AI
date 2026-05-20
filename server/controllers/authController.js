f866e8a54f85eea9c008735d459cbcb9c55c5ac4
Darshan Kumbhar <darshankgithub@gmail.com>
2026-05-21 00:32:10 +0530

    Change SameSite cookie attribute to 'none'

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,7 +13,8 @@ export const goggleAuth = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "lax",
+            sameSite: "none",
+            path:"/"
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
