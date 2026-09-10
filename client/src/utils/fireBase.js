5ab2a10594c64ee8dd791bb141011dab6fd92620
Darshan Kumbhar <darshankgithub@gmail.com>
2026-09-10 11:49:06 +0530

    Fix typos in Firebase config for authDomain and projectId
    Corrected typos in Firebase configuration values.

--- client/src/utils/fireBase.js
+++ client/src/utils/fireBase.js
@@ -3,9 +3,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: "goggleauth-9829b.firebaseapp.com",
-  projectId: "goggleauth-9829b",
-  storageBucket: "goggleauth-9829b.firebasestorage.app",
+  authDomain: "googleauth-9829b.firebaseapp.com",
+  projectId: "googleauth-9829b",
+  storageBucket: "googleauth-9829b.firebasestorage.app",
   messagingSenderId: "819366704844",
   appId: "1:819366704844:web:833b72a46bce019cdab469"
 };
@@ -14,7 +14,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
-
 
 provider.setCustomParameters({
   prompt: "select_account",
